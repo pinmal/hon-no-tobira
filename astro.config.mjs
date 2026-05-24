f133934e55a60964b998a8b998dbdd3dd5ca51bd
Yotsu Kano <pinmal@hotmail.com>
2026-05-24 16:09:21 +0900

    Fix build: remove sitemap plugin, add static sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hon-no-tobira.pages.dev',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   output: 'static',
 });
